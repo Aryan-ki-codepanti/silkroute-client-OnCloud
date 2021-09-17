a9b5fee93f9c6b13420103ea408bc1dd7fbd7099
Aryan Sethi <aryan.rdps@gmail.com>
2021-09-17 16:16:25 +0530

    Added component SignUp along with phone State

---
+++ src/components/OTP.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function OTP() {
+    return (
+        <h1>d</h1>
+    )
+}
---
+++ src/components/SignUp.js
@@ -0,0 +1,47 @@
+import React , {useState} from 'react'
+import heroSignUp from "../img/hero-signup.png"
+
+export default function SignUp(props) {
+
+
+
+    // store mobile number
+    const [phoneNumber, setPhoneNumber] = useState('');
+
+    const handleOnChange = (event)=>{
+        setPhoneNumber(event.target.value);
+    }
+
+    
+
+
+    return (
+
+        <div className="SignUpBox m-auto p-3">
+            <div className="img-box d-flex align-items-center justify-content-center">
+                <img src={heroSignUp} alt="" />
+            </div>
+
+            <div className="content-box d-flex justify-content-center flex-column align-items-center">
+                <h1 className="mb-0">Silk Route</h1>
+                <small>Helping You Sell Better</small>
+            </div>
+
+            <form >
+                <div className="form-box px-3 mt-3">
+
+                    <input type="tel" className="form-control p-2 px-3" placeholder="Enter Phone Number"  value={phoneNumber} onChange={handleOnChange} />
+
+                    <button disabled={phoneNumber.length === 10 && phoneNumber.toString().match(/[0-9]{10}/)?false:true} className="btn w-100 my-4" type="submit">SignUp</button>
+                </div>
+            </form>
+
+            <div className="text-end pb-5">
+                <input type="checkbox" className="form-check-input" name="rememberMe" id="rememberMe"/>
+                <span> Remember me </span>
+            </div>
+
+        </div>
+        
+    )
+}
